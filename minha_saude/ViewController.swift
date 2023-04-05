fa8caf9ef64d6ccadfec86011d4724a621cdb26b
Larissa Diniz Martins <larissa.diniz@ifood.com.br>
2023-04-05 18:28:58 -0300

    Initial Commit

---
+++ minha_saude/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  minha_saude
+//
+//  Created by Larissa Diniz on 05/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
